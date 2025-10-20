248754a1c65166065dd961697ace5a8fc23e606e
Femi <fagbemibabs18@gmail.com>
2025-10-20 20:35:23 +0100

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-xl text-red-500 font-JakartaBold">Services Provider App Demo</Text>
+    </View>
+  );
+}
